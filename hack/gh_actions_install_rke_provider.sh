f4cef50ade29875a329b6cdd58deaa287540de71
Jacob Weinstock <jacob.weinstock@netapp.com>
2020-06-16 12:37:52 -0600

    debugging

--- hack/gh_actions_install_rke_provider.sh
+++ hack/gh_actions_install_rke_provider.sh
@@ -4,5 +4,8 @@ RKE_PROVIDER_VERSION=1.0.0
 
 curl -LO https://github.com/rancher/terraform-provider-rke/releases/download/${RKE_PROVIDER_VERSION}/terraform-provider-rke-linux-amd64.tar.gz
 tar -xzf terraform-provider-rke-linux-amd64.tar.gz
-mkdir -p terraform/vsphere-rancher/terraform.d/plugins/linux_amd64/
-mv terraform-provider-rke-*/terraform-provider-rke terraform/vsphere-rancher/terraform.d/plugins/linux_amd64/terraform-provider-rke
+mkdir -p terraform.d/plugins/linux_amd64/
+mv terraform-provider-rke-*/terraform-provider-rke terraform.d/plugins/linux_amd64/terraform-provider-rke
+
+pwd
+ls -ltrah
