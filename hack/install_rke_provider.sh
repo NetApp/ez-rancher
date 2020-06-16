f9c4d0f62c6efc566f98e59cd0a22bc963ccf551
Jacob Weinstock <jacob.weinstock@netapp.com>
2020-06-16 11:43:07 -0600

    update rke provider install

--- hack/install_rke_provider.sh
+++ hack/install_rke_provider.sh
@@ -4,5 +4,5 @@ RKE_PROVIDER_VERSION=1.0.0
 
 curl -LO https://github.com/rancher/terraform-provider-rke/releases/download/${RKE_PROVIDER_VERSION}/terraform-provider-rke-linux-amd64.tar.gz
 tar -xzf terraform-provider-rke-linux-amd64.tar.gz
-mkdir -p /root/.terraform.d/plugins/linux_amd64
-mv terraform-provider-rke-*/terraform-provider-rke /root/.terraform.d/plugins/linux_amd64/terraform-provider-rke
+mkdir -p ~/.terraform.d/plugins/linux_amd64
+mv terraform-provider-rke-*/terraform-provider-rke ~/.terraform.d/plugins/linux_amd64/terraform-provider-rke
