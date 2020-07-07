9847b0266afaf27f168938472bda6c0f16104ca6
Ben Gentry <ben.gentry@netapp.com>
2020-07-07 13:02:34 -0600

    Format changes

--- terraform/modules/infrastructure/vsphere/10-data.tf
+++ terraform/modules/infrastructure/vsphere/10-data.tf
@@ -31,26 +31,26 @@ data "template_file" "metadata" {
   template = file("${path.module}/cloudinit/templates/metadata.tpl")
   vars = {
     hostname = format("${var.vm_name}-${var.type}%02d", count.index + 1)
-    netplan = data.template_file.netplan[count.index].rendered
+    netplan  = data.template_file.netplan[count.index].rendered
   }
 }
 
 data "template_file" "userdata" {
-  count = var.node_count
+  count    = var.node_count
   template = file("${path.module}/cloudinit/kickstart-userdata.yaml")
   vars = {
     ssh_public_key = file(var.ssh_public_key)
-    netplan = base64encode(data.template_file.netplan[count.index].rendered)
+    netplan        = base64encode(data.template_file.netplan[count.index].rendered)
   }
 }
 
 data "template_file" "netplan" {
-  count = var.node_count
+  count    = var.node_count
   template = file("${path.module}/cloudinit/templates/netplan.tpl")
   vars = {
-    dhcp_enabled   = local.num_addresses == 0
-    dns_servers    = format("%s [%s]", "addresses:", join(",", var.dns_servers))
-    addresses      = local.num_addresses > 0 ? format("%s %s", "addresses:", jsonencode([var.static_ip_addresses[count.index]])) : ""
-    gateway        = var.default_gateway != "" ? format("%s %s", "gateway4:", var.default_gateway) : ""
+    dhcp_enabled = local.num_addresses == 0
+    dns_servers  = format("%s [%s]", "addresses:", join(",", var.dns_servers))
+    addresses    = local.num_addresses > 0 ? format("%s %s", "addresses:", jsonencode([var.static_ip_addresses[count.index]])) : ""
+    gateway      = var.default_gateway != "" ? format("%s %s", "gateway4:", var.default_gateway) : ""
   }
 }
